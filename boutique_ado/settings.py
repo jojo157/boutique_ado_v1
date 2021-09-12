27a1ac9d7ce8815dc84926b995c5bdcd69827a98
jojo157 <carrigj@tcd.ie>
2021-09-12 08:22:12 +0000

    fixed settings page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,19 +26,6 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['boutique-ado-jc.herokuapp.com', 'localhost']
 
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+ao89u_wre&*f%95_j7rrlz@tm43*#4$3h)_^w_-j$ir=b*(t'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
